d82e74bcbe93a2e89c6ca1bd0ade305016118d95
baishuang <baishuang@yit.com>
2020-09-03 19:04:34 +0800

    swift tool project create

---
+++ BHXSwiftTool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BHXSwiftTool
+//
+//  Created by yit on 2020/9/3.
+//  Copyright © 2020 baishuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
